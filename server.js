3da358aa30651cdb473de28859021cb6307da436
Rhayser Montoya <rhaysermontoya@gmail.com>
2023-04-30 23:09:23 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,90 @@
+import express, { json } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import DataSchema from "./DataSchema.js";
+
+const DataModel = mongoose.model("DataModel", DataSchema);
+
+//Call dotenv config object
+dotenv.config();
+
+//Instantiate express object/function to the constant server
+const server = express();
+server.use(json());
+
+const PORT = process.env.SERVER_PORT;
+
+mongoose.connect(process.env.MONGO_PASS).then(() => {
+  server.listen(PORT, (req, res) => {
+    console.log(`Alive on port ${PORT} and connected to Database`);
+  });
+});
+
+server.get("/authors", async (_, res) => {
+  try {
+    const authors = await DataModel.find();
+    res.status(200).json(authors);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json({ message: "Upps! an error on our side" });
+  }
+});
+
+server.get("/author/:id", async (req, res) => {
+  try {
+    const author = await DataModel.findById(req.params.id);
+    if (!author) {
+      return res
+        .status(404)
+        .json({ message: `Not author found by id: ${req.params.id}` });
+    }
+    res.status(200).json(author);
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error " });
+  }
+});
+
+server.post("/authors", async (req, res) => {
+  try {
+    const newAuthor = new DataModel(req.body);
+    await newAuthor.save();
+    res.status(200).json({ message: "Author Created succesfully" });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: `Could not create author error: ${err}` });
+  }
+});
+
+server.put("/author/:id", async (req, res) => {
+  try {
+    const author = await DataModel.findById(req.params.id);
+    if (!author) {
+      return res.status(404).json({ message: "Author not found, cannot edit" });
+    } else {
+      author.name = req.body.name;
+      author.surname = req.body.surname;
+      author.email = req.body.email;
+      author.dateOfBirth = req.body.dateOfBirth;
+      await author.save();
+      res.status(200).json({ message: "Author updated successfully" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: `Internal server error: ${err}` });
+  }
+});
+
+server.delete("/author/:id", async (req, res) => {
+  try {
+    const author = await DataModel.deleteOne({ _id: req.params.id });
+    if (!author) {
+      return res.status(404).json(`Not author found by id: ${req.params.id}`);
+    }
+
+    res.json({ message: "Author deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: `Internal server error: ${err}` });
+  }
+});
